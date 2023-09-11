17fea361707777b9f79103207e96f21ec653baa9
nicodeheza <nicolasdeheza@hotmail.com>
2023-09-11 18:30:36 -0300

    fix requestAnimationFrame

--- example/src/worker/main.worker.ts
+++ example/src/worker/main.worker.ts
@@ -1,13 +1,23 @@
+import Player from '../../../src/index'
+
 const w = 600
 const h = 600
 
 self.onmessage = async (event) => {
 	console.log(event)
 	if (event.data.view) {
+		const player = new Player(
+			'https://w3c.github.io/webcodecs/samples/data/bbb_video_avc_frag.mp4',
+			true
+		)
 		const canvas = event.data.view
 		canvas.width = w
 		canvas.height = h
-		// const ctx = canvas.getContext('2d')
+		const ctx = canvas.getContext('2d')
+		player.onFrame = (frame) => {
+			ctx.drawImage(frame, 0, 0, w, h)
+			frame?.close()
+		}
 	}
 }
 export {}
